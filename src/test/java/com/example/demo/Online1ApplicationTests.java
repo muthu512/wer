6da316b467edae533aabf7e5abc5f40f2a4447fb
muthu512 <itzsmk@gmail.com>
2024-10-25 11:27:26 +0530

    edit

--- src/test/java/com/example/demo/Online1ApplicationTests.java
+++ src/test/java/com/example/demo/Online1ApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(exclude = { DataSourceAutoConfiguration.class }) // Exclude DataSource configuration
 class Online1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test will now run without trying to configure a data source
+    }
 
 }
